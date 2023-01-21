0fd851801f358058d67c28cce927b235f2110931
Nguyễn Lâm Tùng <103561521+tungnguyen0304@users.noreply.github.com>
2023-01-21 23:05:01 +0700

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -17,7 +17,8 @@ def play():
 def is_win(player, opponent):
     # return true if player wins
     # r > s, s > p, p > r
-    if(player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+    if(player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
         return True
         
 print(play())
